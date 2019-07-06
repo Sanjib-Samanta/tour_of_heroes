71de344363085e84392c319b8ef78fd0e3a833ad
Sanjib-Samanta <sanjib.samanta@gmail.com>
2019-07-06 17:25:42 -0500

    first commit of the Avengers

---
+++ src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Hero } from '../hero';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { HeroService } from '../hero.service';
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css']
+})
+export class HeroDetailComponent implements OnInit {
+@Input() hero: Hero;
+imageUrl;
+  constructor(private route: ActivatedRoute,
+              private heroService: HeroService,
+              private location: Location) { }
+
+  ngOnInit() {
+    this.getHero();
+  }
+  getHero(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
+    this.imageUrl = 'assets\\' + id + '.png';
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+}
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 10, name: 'Nick Fury' },
+  { id: 11, name: 'Iron Man' },
+  { id: 12, name: 'Captain America' },
+  { id: 13, name: 'Hulk' },
+  { id: 14, name: 'Thor' },
+  { id: 15, name: 'Black Widow' },
+  { id: 16, name: 'Hawk Eye' },
+  { id: 17, name: 'Spider-Man' },
+  { id: 18, name: 'Dr. Stranger' },
+  { id: 19, name: 'Black Panther' },
+  { id: 20, name: 'Captain Marvel' }
+];
